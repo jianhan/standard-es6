6e7e1c6d0631a58c38a2c2511d7a6c2c5c59b2aa
Jian Han <jianhan@mail.com>
2018-05-11 23:21:22 +1000

    Fill

---
+++ ch06_array/entries_keys_values.js
@@ -0,0 +1,24 @@
+// S6 提供三个新的方法—— entries() ， keys() 和 values() ——用于遍历
+// 数组。它们都返回一个遍历器对象（ 详见《 Iterator》 一章） ，可以用 for...of 循环进行遍历
+
+for (let index of ['a', 'b'].keys()) {
+console.log(index);
+}
+// 0
+// 1
+for (let elem of ['a', 'b'].values()) {
+console.log(elem);
+}
+// 'a'
+// 'b'
+for (let [index, elem] of ['a', 'b'].entries()) {
+console.log(index, elem);
+}
+// 0 "a"
+// 1 "b"
+
+let letter = ['a', 'b', 'c'];
+let entries = letter.entries();
+console.log(entries.next().value); // [0, 'a']
+console.log(entries.next().value); // [1, 'b']
+console.log(entries.next().value); // [2, 'c']
